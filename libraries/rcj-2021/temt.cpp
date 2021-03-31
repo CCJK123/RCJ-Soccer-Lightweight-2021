3998d03a290817041f6189725ead958e467a1736
PorridgePi <kunbao2006@126.com>
2021-03-31 23:09:05 +0800

    lib(temt): Add files

---
+++ libraries/rcj-2021/temt.cpp
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+#include "temt.h"
+
+Temt::Temt(int pin, int threshold) {
+  pinMode(pin, INPUT);
+  _pin = pin;
+  _threshold = threshold;
+}
+
+int Temt::update() {
+  return analogRead(_pin);
+}
+
+double Temt::updatePercent() {
+  return double(analogRead(_pin))/_threshold;
+}
