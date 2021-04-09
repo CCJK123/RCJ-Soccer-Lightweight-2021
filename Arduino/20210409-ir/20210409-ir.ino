267d2dfab126ab80bcb335ae79305b235c17fa4d
PorridgePi <www.kunbao2006@gmail.com>
2021-04-09 15:18:51 +0800

    test(lib): ir: Update to use class

--- Arduino/20210409-ir/20210409-ir.ino
+++ Arduino/20210409-ir/20210409-ir.ino
@@ -1,19 +1,40 @@
 #include <Wire.h> 
 
+class IR {
+  public:
+    IR() {
+     Wire.begin();
+    }
+    
+    int one(int channel) {  
+        Wire.beginTransmission(0x01);
+        Wire.write(channel); Wire.endTransmission();
+        Wire.requestFrom(0x01,1);
+        
+        while(!Wire.available());
+        return Wire.read();
+    }
+    
+    int maximum() {
+      return one(9);
+    }
+    
+    int maximum_ch() {
+      return one(8);
+    }
+};
+
+IR ir;
+
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin();
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Wire.beginTransmission(0x01);
-  Wire.write(8); Wire.endTransmission();
-  Wire.requestFrom(0x01,1);
-  while(!Wire.available());
-  Serial.println(Wire.read());
-  delay(100);
+  Serial.println(ir.maximum());
+  delay(10);
 } 
 
 // yellow scl a5
