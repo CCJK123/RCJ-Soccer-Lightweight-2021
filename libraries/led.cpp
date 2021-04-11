ff7ea0f891c75e5e866f8262348ef080ecf8d22e
Christopher Cheng <chris.chengjk@gmail.com>
2021-04-11 23:13:03 +0800

    Modified implementation

--- libraries/led.cpp
+++ libraries/led.cpp
@@ -3,9 +3,10 @@
 #include <FastLED.h>
 
 LED::LED(int pin) {
+  CRGB leds[32];
   FastLED.addLeds<WS2812, pin, GRB>(leds, 32);
   for (int i = 0; i <= 32; i++) {
-    leds[i] = CRGB(255, 255, 255);
+    leds[i] = CRGB::White;  //CRGB(255, 255, 255) also works
   }
   FastLED.show();
   _pin = pin;
