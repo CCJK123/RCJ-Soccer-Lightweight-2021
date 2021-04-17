2a72bd8df45aa0098b0a473b371050ec3837710d
PorridgePi <kunbao2006@126.com>
2021-04-17 09:21:12 +0800

    test(lib): led: Update to use library

--- test/libraries/led/led.ino
+++ test/libraries/led/led.ino
@@ -1,27 +1,4 @@
-#include <FastLED.h>
-
-template <int pin>
-class LED {
-  public:
-    LED() {
-      FastLED.addLeds<WS2812, pin, GRB>(leds, 32);
-    }
-
-    void on() {
-      for (int i = 0; i <= 32; i++) {
-        leds[i] = CRGB::White;  // CRGB(255, 255, 255) also works
-      }
-      FastLED.show();
-    }
-
-    void off() {
-      FastLED.clear();
-      FastLED.show();
-    }
-
-  private:
-    CRGB leds[32];
-};
+#include <led.h>
 
 LED<6> led;
 
@@ -29,8 +6,8 @@ void setup() {
 }
 
 void loop() {
-  led.on();
+//  led.on();
   delay(1000);
-  led.off();
+//  led.off();
   delay(1000);
 }
