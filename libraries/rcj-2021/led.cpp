9adf94089532bc544054ee3d46c56993642f55ec
Christopher Cheng <chris.chengjk@gmail.com>
2021-05-05 17:18:40 +0800

    Update no. of LEDs

--- libraries/rcj-2021/led.cpp
+++ libraries/rcj-2021/led.cpp
@@ -2,13 +2,13 @@
 #include <Adafruit_NeoPixel.h>
 #include "led.h"
 
-LED::LED(int pin) : Adafruit_NeoPixel(32, pin, NEO_GRB + NEO_KHZ800) {
+LED::LED(int pin) : Adafruit_NeoPixel(24, pin, NEO_GRB + NEO_KHZ800) {
   Adafruit_NeoPixel::begin();
   _pin = pin;
 }
 
 void LED::on() {
-  for (int i = 0; i < 32; i++) {
+  for (int i = 0; i < 24; i++) {
     Adafruit_NeoPixel::setPixelColor(i, 255, 255, 255);
   }
   Adafruit_NeoPixel::show();
