80e73d5d36404e26cd63185f055249c7d92d457b
Christopher Cheng <chris.chengjk@gmail.com>
2021-05-07 16:07:38 +0800

    Modified ir library to work with 2 Wire and Wire1

--- test/libraries/ir/ir.ino
+++ test/libraries/ir/ir.ino
@@ -7,17 +7,19 @@ Brown - SDA - Arduino A4 Pin
 */
 
 IR ir;
+int i2cPort = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  ir.begin(i2cPort);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print(ir.maxVal());
+  Serial.print(ir.maxVal(i2cPort));
   Serial.print(" ");
-  Serial.print(ir.maxChannel());
+  Serial.print(ir.maxChannel(i2cPort));
   Serial.print("\n");
   delay(100);
 } 
