979b755cf618428adabbe2a3b7acf786c06b9480
PorridgePi <kunbao2006@126.com>
2021-11-11 19:16:27 +0800

    lib(multiplexer): feat: Add completed library

---
+++ libraries/rcj-2021/multiplexer.h
@@ -0,0 +1,19 @@
+// Library for the Adafruit TCA9548A 1-to-8 I2C Multiplexer Breakout
+// Referenced https://learn.adafruit.com/adafruit-tca9548a-1-to-8-i2c-multiplexer-breakout/wiring-and-test
+
+#include "Arudino.h"
+#include <Wire.h>
+
+#ifndef multiplexer_h
+#define multiplexer_h
+
+class Multiplexer {
+    public:
+        Multiplexer(uint8_t address);
+        void select(uint8_t channel);
+        void stop();
+    private:
+        uint8_t _address;
+}
+
+#endif
