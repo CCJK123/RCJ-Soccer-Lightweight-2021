fb9646299628266983aae3e8b6047f608f616cec
PorridgePi <kunbao2006@126.com>
2021-11-13 22:35:52 +0800

    main-logic: !fix: Flipped coordOrigin and coordDest

--- main-logic/striker/striker.ino
+++ main-logic/striker/striker.ino
@@ -226,17 +226,17 @@ void moveCornerToCoord(int coordOrigin[4][2], int coordDest[2]) {
   double moveAngle;
   worstCorner = 0;
   // removed sqrt from distance calculation as unnecessary
-  worstCornerDist = pow((coordOrigin[0] - coordDest[0][0]), 2) + pow((coordOrigin[0] - coordDest[0][1]), 2);
+  worstCornerDist = pow((coordDest[0] - coordOrigin[0][0]), 2) + pow((coordDest[0] - coordOrigin[0][1]), 2);
   
   for (int i=1; i<4; i++) {
-    currentCornerDist = pow((coordOrigin[0] - coordDest[i][0]), 2) + pow((coordOrigin[0] - coordDest[i][1]), 2);
+    currentCornerDist = pow((coordDest[0] - coordOrigin[i][0]), 2) + pow((coordDest[0] - coordOrigin[i][1]), 2);
     if (currentCornerDist < worstCornerDist) {
       worstCornerDist = currentCornerDist;
       worstCorner = i;
     }
   }
   
-  moveAngle = atan2(coordOrigin[1]-coordDest[worstCorner][1], coordOrigin[0]-coordDest[worstCorner][0]);
+  moveAngle = atan2(coordDest[1]-coordOrigin[worstCorner][1], coordDest[0]-coordOrigin[worstCorner][0]);
   switch (worstCorner) {
     case 0:
       // Front Left Corner, Move Back Right
